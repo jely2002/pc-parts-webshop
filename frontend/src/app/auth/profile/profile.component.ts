9e9e5f2c0b850d909f0e991a0a52819a3aeeee53
Jelle Glebbeek <jw.glebbeek@gmail.com>
2022-01-21 23:15:35 +0100

    :sparkles: add simple WIP profile page

--- frontend/src/app/auth/profile/profile.component.ts
+++ frontend/src/app/auth/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
+import { UserModel } from "../user.model";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  user: UserModel;
 
   ngOnInit(): void {
+    this.authService.checkIfLoggedIn().subscribe({
+      next: user => {
+        this.user = user;
+      },
+      error: () => {
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['']);
+    });
   }
 
 }
