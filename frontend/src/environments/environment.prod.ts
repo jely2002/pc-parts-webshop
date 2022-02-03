5eaa9573d0c1b5bbda91747128c01994002be6a3
Jelle Glebbeek <jw.glebbeek@gmail.com>
2022-02-03 04:31:30 +0100

    :beers: forgot to commit this code

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://pcparts-api-pjmso.ondigitalocean.app/api',
   captchaKey: '6LcjXVQeAAAAAGRbVYtio58T4TaLE8XNtzZ_nfVu',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://pcparts-api-pjmso.ondigitalocean.app/api',
   captchaKey: '6LcjXVQeAAAAAGRbVYtio58T4TaLE8XNtzZ_nfVu',
 };
 
